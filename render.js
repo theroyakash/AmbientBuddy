afcb05f8fa1ca2a451e41c7724290dca351ce911
amder111 <52421748+amder111@users.noreply.github.com>
2023-06-16 17:06:20 +0530

    generic function create for individual audio segments

--- render.js
+++ render.js
@@ -53,3 +53,45 @@ let abusivevolume = document.querySelector("#volume-control-abusive");
 abusivevolume.addEventListener("change", function (event) {
     abusiveAudio.volume = event.currentTarget.value / 100;
 });
+
+
+// generic function 
+var audioStateStore = new Map();
+
+function audioTogglePlay(id) {
+    var audio = document.getElementById(id + "_audio");
+    var audio_button = document.getElementById(id + "_audio_button");
+    
+    if (!audioStateStore.has(id + "_audioIsPlaying")) {
+        audioStateStore[id + "_audioIsPlaying"] = false;
+    }
+
+    var audio_state = audioStateStore[id + "_audioIsPlaying"];
+
+    if (audio_state) {
+        audio.pause();
+        audio_button.innerHTML = "Start Playing";
+    } else {
+        audio.play();
+        audio_button.innerHTML = "Stop Playing";
+    }
+
+    audio.onplaying = function () {
+        audioStateStore[id + "_audioIsPlaying"] = true;
+    };
+
+    audio.onpause = function () {
+        audioStateStore[id + "_audioIsPlaying"] = false;
+    };
+
+    let volume_service = document.querySelector("#" + id + "_volume_control");
+    console.log(volume_service);
+    
+    setupVolumeControlEventListener(audio, volume_service);
+};
+
+function setupVolumeControlEventListener(audio, volume_service) {
+    volume_service.addEventListener("change", function (event) {
+        audio.volume = event.currentTarget.value / 100;
+    });
+}
